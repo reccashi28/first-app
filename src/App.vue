f438020c08d107c5d01ae0f827496d35c3ec436a
reccashi28 <faustorebeccabsit@gmail.com>
2024-02-03 15:17:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Hello {{ name }}</p>
+  <p v-text="channel"></p>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: "Rebecca 2",
+      channel: "Illongga Frau"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
